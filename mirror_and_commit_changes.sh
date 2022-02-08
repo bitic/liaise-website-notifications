db347110c7682c0c2dfde4a5ad4143de7dd2d49f
Pere Quintana <pquintana@obsebre.es>
2022-02-08 11:03:55 +0100

    ups

--- mirror_and_commit_changes.sh
+++ mirror_and_commit_changes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 site_url=https://www.hymex.fr/liaise/
 rm -f www.hymex.fr/liaise/index.html # this way it checks all the child links, otherwise, if index.html doesn't change, the other links are not checked for changes.
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent http://example.org $site_url 
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent $site_url 
 git add *.html
 git commit -m "website update"
+git push origin master
 
