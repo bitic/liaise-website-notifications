4710004a59eeb50d076959136f10924a4faf1c96
Pere Quintana <pquintana@obsebre.es>
2022-02-08 11:11:03 +0100

    comment

--- mirror_and_commit_changes.sh
+++ mirror_and_commit_changes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this script mirrors the website and commits any changes to github
 site_url=https://www.hymex.fr/liaise/
 rm -f www.hymex.fr/liaise/index.html # this way it checks all the child links, otherwise, if index.html doesn't change, the other links are not checked for changes.
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent $site_url 
