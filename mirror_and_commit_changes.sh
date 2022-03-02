6cee0a17a5e50eff34384ffba6228c6f9bfa6860
Pere Quintana <pquintana@obsebre.es>
2022-03-02 14:01:53 +0100

    millora per quan falla la connexió

--- mirror_and_commit_changes.sh
+++ mirror_and_commit_changes.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # this script mirrors the website and commits any changes to github
 site_url=https://www.hymex.fr/liaise/
-rm -f www.hymex.fr/liaise/index.html # this way it checks all the child links, otherwise, if index.html doesn't change, the other links are not checked for changes.
+indexfile=www.hymex.fr/liaise/index.html
+mv $indexfile $indexfile.old # this way it checks all the child links, otherwise, if index.html doesn't change, the other links are not checked for changes.
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent $site_url 
+if [ ! -e "$indexfile" ]; then # if there is no index, (wget has failed du to a network problem)
+	mv $indexfile.old $indexfile
+else
+	rm -f $indexfile.old
+fi
 git add *.html
 git commit -m "website update"
 git push origin master
